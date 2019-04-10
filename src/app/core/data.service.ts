5b10f4f97437a67996e858943b058ee113743a71
Michael Thomas <m@msthomas.xyz>
2019-04-10 12:13:34 +0100

    display of transaction type

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -55,10 +55,44 @@ export class DataService {
     }
 
     getTransactions(account): Observable<any> {
+      // map of types to icon
+      const icons = {
+        'MASTER_CARD' : 'credit-card',
+        'MASTERCARD_MONEYSEND' : 'credit-card',
+        'MASTERCARD_CHARGEBACK' : 'credit-card',
+        'STRIPE_FUNDING' : 'plus-circle',
+        'STARLING_PAY_STRIPE' : 'plus-circle',
+        'CASH_DEPOSIT' : 'money-bill-wave',
+        'CASH_WITHDRAWAL' : 'money-bill-wave',
+        'CHAPS' : 'university',
+        'CURRENCY_CLOUD' : 'university',
+        'DIRECT_CREDIT' : 'university',
+        'DIRECT_DEBIT' : 'university',
+        'DIRECT_DEBIT_DISPUTE' : 'university',
+        'INTERNAL_TRANSFER' : 'university',
+        'FASTER_PAYMENTS_IN' : 'university',
+        'FASTER_PAYMENTS_OUT' : 'university',
+        'FASTER_PAYMENTS_REVERSAL' : 'university',
+        'FASTER_PAYMENTS_REFUND' : 'university',
+        'SEPA_CREDIT_TRANSFER' : 'university',
+        'SEPA_DIRECT_DEBIT' : 'university',
+        'CASH_WITHDRAWAL_CHARGE': 'coins', 
+        'CASH_DEPOSIT_CHARGE': 'coins', 
+        'INTEREST_PAYMENT': 'percentage', 
+        'OVERDRAFT': 'percentage', 
+        'OVERDRAFT_INTEREST_WAIVED': 'percentage', 
+        'LOAN_PRINCIPAL_PAYMENT': 'percentage', 
+        'LOAN_REPAYMENT': 'percentage', 
+        'LOAN_OVERPAYMENT': 'coins', 
+        'LOAN_LATE_PAYMENT': 'coins'
+      };
       return this.http.get('/api/v2/feed/account/'+account.accountUid+'/category/'+account.defaultCategory)
       .pipe(
-        map( resp => {
-          return resp.feedItems
+        map( resp=> {
+          resp.feedItems.map(function(item){
+            item.icon = icons[item.source] || 'receipt';
+          })
+          return resp.feedItems;
         }),
         catchError(this.handleError)
       );
