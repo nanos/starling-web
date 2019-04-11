8afbdc6f53169ebe5d6a68e09c1498463e82a568
Michael Thomas <m@msthomas.xyz>
2019-04-11 11:31:31 +0100

    Move icon stuff out of data service and into transaction component

--- src/app/accounts/transactions/transactions.component.ts
+++ src/app/accounts/transactions/transactions.component.ts
@@ -9,6 +9,38 @@ import { ITransaction, IAccount } from '../../models/interfaces.model';
   styleUrls: ['./transactions.component.css']
 })
 export class AccountsTransactionsComponent implements OnInit {
+  // map of types to icon
+  icons = {
+    'MASTER_CARD' : 'credit-card',
+    'MASTERCARD_MONEYSEND' : 'credit-card',
+    'MASTERCARD_CHARGEBACK' : 'credit-card',
+    'STRIPE_FUNDING' : 'plus-circle',
+    'STARLING_PAY_STRIPE' : 'plus-circle',
+    'CASH_DEPOSIT' : 'money-bill-wave',
+    'CASH_WITHDRAWAL' : 'money-bill-wave',
+    'CHAPS' : 'university',
+    'CURRENCY_CLOUD' : 'university',
+    'DIRECT_CREDIT' : 'university',
+    'DIRECT_DEBIT' : 'university',
+    'DIRECT_DEBIT_DISPUTE' : 'university',
+    'INTERNAL_TRANSFER' : 'university',
+    'FASTER_PAYMENTS_IN' : 'university',
+    'FASTER_PAYMENTS_OUT' : 'university',
+    'FASTER_PAYMENTS_REVERSAL' : 'university',
+    'FASTER_PAYMENTS_REFUND' : 'university',
+    'SEPA_CREDIT_TRANSFER' : 'university',
+    'SEPA_DIRECT_DEBIT' : 'university',
+    'CASH_WITHDRAWAL_CHARGE': 'coins', 
+    'CASH_DEPOSIT_CHARGE': 'coins', 
+    'INTEREST_PAYMENT': 'percentage', 
+    'OVERDRAFT': 'percentage', 
+    'OVERDRAFT_INTEREST_WAIVED': 'percentage', 
+    'LOAN_PRINCIPAL_PAYMENT': 'percentage', 
+    'LOAN_REPAYMENT': 'percentage', 
+    'LOAN_OVERPAYMENT': 'coins', 
+    'LOAN_LATE_PAYMENT': 'coins'
+  };
+
   private _account: IAccount;
   filteredTransactions: ITransaction[];
 
@@ -34,7 +66,10 @@ export class AccountsTransactionsComponent implements OnInit {
     this.isOpen = !this.isOpen;
     
     this.dataService.getTransactions(this.account)
-        .subscribe( (transactions:ITransaction[]) => this.transactions = this.filteredTransactions = transactions );
+        .subscribe( (transactions:any[]) => this.transactions = this.filteredTransactions = transactions.map((trans) => {
+          trans.icon = this.icons[trans.source] || 'receipt';
+          return trans;
+        }));
   }
 
   filter(data: string) {
