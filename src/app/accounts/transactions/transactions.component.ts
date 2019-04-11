1419aa235de580cfa03f9c55473816095347486e
Michael Thomas <m@msthomas.xyz>
2019-04-11 10:03:57 +0100

    tidy up

--- src/app/accounts/transactions/transactions.component.ts
+++ src/app/accounts/transactions/transactions.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input, HostBinding } from '@angular/core';
-// import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { DataService } from '../../core/data.service';
-import { ITransaction } from '../../models/interfaces.model';
+import { ITransaction, IAccount } from '../../models/interfaces.model';
 
 @Component({
   selector: 'app-accounts-transactions',
@@ -10,16 +9,16 @@ import { ITransaction } from '../../models/interfaces.model';
   styleUrls: ['./transactions.component.css']
 })
 export class AccountsTransactionsComponent implements OnInit {
-	private _account: any[] = [];
+	private _account: IAccount;
   filteredTransactions: ITransaction[];
 
   transactions: ITransaction[] = [];
   
-  @Input() get account(): any[] {
+  @Input() get account(): IAccount {
 		return this._account;
 	}
 
-	set account(value: any[]) {
+	set account(value: IAccount) {
 		if (value) {
 			this._account = value;
 		}
@@ -35,13 +34,12 @@ export class AccountsTransactionsComponent implements OnInit {
     this.isOpen = !this.isOpen;
     
     this.dataService.getTransactions(this.account)
-        .subscribe( (transactions:any[]) => this.transactions = this.filteredTransactions = transactions );
+        .subscribe( (transactions:ITransaction[]) => this.transactions = this.filteredTransactions = transactions );
   }
 
   filter(data: string) {
         if (data) {
             this.filteredTransactions = this.transactions.filter((trans:ITransaction) => {
-                console.log(trans.counterPartyName, data, trans.counterPartyName.toLowerCase().indexOf(data.toLowerCase()));
                 return trans.counterPartyName.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
                        trans.reference.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
                        trans.amount.minorUnits == parseInt(data) * 100 ;
