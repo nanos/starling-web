f46a5e2b520f63225b19b45187452ebaad1201c3
Michael Thomas <m@msthomas.xyz>
2019-04-12 11:45:16 +0100

    maintain sorting of accounts

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -13,18 +13,27 @@ export class AccountsComponent implements OnInit {
   title: string;
   baseUrl: string;
   accounts: IAccount[] = [] ;
+  // to mantain order regardless of network conditions:
+  // keep an array of access keys and sort the accounts by them
+  order: string[] = [];
 
   constructor( private dataService: DataService ) { }
 
   ngOnInit() {
     for( let key of AppConfig.settings.api_keys ) {
-      this.dataService.getAccounts( key )
-          .subscribe( (accounts:IAccount[]) => {
-            for( let acc of accounts ) {
-              acc.api_key = key;
-              this.accounts.push(acc);
-            }
-          });
+      
+      // keep sequenc of api keys so we can sort by it
+      this.order.push(key);
+
+      this.dataService.getAccounts( key ).subscribe( (accounts:IAccount[]) => {
+        this.accounts = this.accounts.concat(accounts);
+        // now sort accounts;
+        this.accounts.sort( (a:IAccount, b:IAccount) => {
+          const aIndex = this.order.indexOf( a.api_key );
+          const bIndex = this.order.indexOf( b.api_key );
+          return aIndex - bIndex;
+        })
+      });
     }
 
   }
